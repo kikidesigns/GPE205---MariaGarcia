9fb7124fb30b2a4d84efd0a5c1411c9880b55aa3
kikidesigns <aquarianprojectmgmt@gmail.com>
2024-04-01 23:08:24 -0500

    player controller
    created player controller script

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : ControllerScript
+{
+    public KeyCode moveForwardKey;
+    public KeyCode moveBackwardKey;
+    public KeyCode rotateClockwiseKey;
+    public KeyCode rotateCounterClockwiseKey;
+   
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+   public override void Update()
+    {
+        base.Update();
+    }
+    public override void ProcessInputs()
+    {
+        if (Input.GetKey(moveforwardKey))
+        {
+            pawn.MoveForward();
+        }
+        if(Input.GetKey(moveBackwardKey)
+        {
+            pawn.MoveBackward();
+        })
+        if(Input.GetKey(rotateClockwiseKey))
+        {
+            pawn.RotateClockwise();
+        }
+        If(Input.GetKey(rotateCounterClockwiseKey))
+        {
+            pawn.RotateCounterClockwise();
+        }
+    }
+}
