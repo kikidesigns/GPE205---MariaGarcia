b5d8089f27efbce5bf4e6466f790b44aaa602e49
kikidesigns <aquarianprojectmgmt@gmail.com>
2024-04-02 13:36:22 -0500

    created components
    Mover and TankMover

---
+++ Assets/Scripts/TankMover.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMover : Mover
+{
+    //Variable for rigidbody component
+    private Rigidbody rb;
+    //start called before first frame update
+    public override void Start()
+    {
+        //get rigidbody component
+        rb = GetComponent<Rigidbody>();
+    }
+    public override void Move(Vector3 direction, float speed)
+    {
+        Vector3 moveVector = direction.normalized * speed * Time.deltaTime;
+        rb.MovePosition(rb.position + moveVector);
+    }
+}
