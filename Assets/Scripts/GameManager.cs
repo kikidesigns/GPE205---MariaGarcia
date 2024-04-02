b82edbe1478ef4550cf08027530b27b3489837f2
kikidesigns <aquarianprojectmgmt@gmail.com>
2024-04-02 16:17:52 -0500

    created game manager
    component created and attached to gameobject

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    // awake is called before start can run
+    private void Awake()
+    {
+       //if the instance doesnt exist yet
+       if(instance == null)
+       {
+        //this is the instance
+        instance = this;
+        //dont destroy it if we load a new scene
+        DontDestroyOnLoad (gameObject);
+       }
+       else 
+       {
+            //otherwise there is already an instance so destroy this gameobject
+            Destroy(gameObject);
+       }
+    }
+    
+}
