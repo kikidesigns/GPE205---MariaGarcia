a7c2b099918e98c41d7fbdd8a8cbb0fadd619134
kikidesigns <aquarianprojectmgmt@gmail.com>
2024-04-14 17:44:26 -0500

    shootkey added
    added to controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : Controller
     public KeyCode moveBackwardKey;
     public KeyCode rotateClockwiseKey;
     public KeyCode rotateCounterClockwiseKey;
+    public KeyCode shootKey;
 
     // Start is called before the first frame update
     public override void Start()
@@ -59,6 +60,11 @@ public class PlayerController : Controller
         if (Input.GetKey(rotateCounterClockwiseKey)) 
         {
             pawn.RotateCounterClockwise();
+        }
+
+        if (Input.GetKeyDown(shootKey))
+        {
+            pawn.shoot();
         }
     }
         public new void OnDestroy()
