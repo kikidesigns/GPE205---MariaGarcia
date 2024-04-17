32ad45d61309adb13a82c186a8e059981797cb2b
kikidesigns <aquarianprojectmgmt@gmail.com>
2024-04-17 12:30:03 -0500

    added enum and some state methods
    switchcase to make decisions and do XState methods

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -8,6 +8,12 @@ public class AIController : Controller
     //variable to hold state changes
     private float lastStateChangeTime;
 
+    //define enum
+    public enum AIState {Idle, GuardDesert, WormAttack, WormRest, GuardSpice, Defend, Scan, Chase, Attack, Flee, BackToPost, Steal, Mine};
+
+    //create a variable of this enum type
+    public AIState currentState;
+
     // Start is called before the first frame update
     public override void Start()
     {      
@@ -33,6 +39,64 @@ public class AIController : Controller
     public void MakeDecisions()
     {
         Debug.Log("Making Decisions");
+
+        // switch/case decision structure
+        switch (currentState) 
+        {
+            //Idle
+            case AIState.Idle:
+            Debug.Log("Idling");
+            break;
+            //GuardDesert
+            case AIState.GuardDesert:
+            Debug.Log("A Sandworm is out there listening");
+            break;
+            //WormAttack
+            case AIState.WormAttack:
+            Debug.Log("A Sandworm is coming");
+            break;
+            //WormRest
+            case AIState.WormRest:
+            Debug.Log("The Sandworm has left");
+            break;
+            //GuardSpice 
+            case AIState.GuardSpice:
+            Debug.Log("Guarding spice");
+            break;
+            //Defend
+            case AIState.Defend:
+            Debug.Log("Self defense");
+            break;
+            //Scan
+            case AIState.Scan:
+            Debug.Log("Scanning perimeter");
+            break;
+            //Chase
+            case AIState.Chase:
+            Debug.Log("Chasing Fremen");
+            break;
+            //Attack
+            case AIState.Attack:
+            Debug.Log("Attacking Fremen");
+            break;
+            //Flee
+            case AIState.Flee:
+            Debug.Log("Fly away from Sandworm");
+            break;
+            //BackToPost
+            case AIState.BackToPost:
+            Debug.Log("Going back to base");
+            break;
+            //Steal
+            case AIState.Steal:
+            Debug.Log("Stealing spice");
+            break;
+            //Mine
+            case AIState.MineSpice:
+            Debug.Log("Mining spice");
+            break;
+
+        }
     }
 
     public virtual void ChangeState (AIState newState)
@@ -42,4 +106,27 @@ public class AIController : Controller
         //save the time we changed states
         lastStateChangeTime = Time.time;
     }
+
+    protected void DoIdleState()
+    {
+        //Do Nothing
+    }
+
+    protected void DoGuardDesertState()
+    {
+        //invisible
+    }
+
+    public void Invisible()
+    {
+        //set visibility to negative
+    }
+
+    protected void DoWormAttackState()
+    {
+        //rotate in direction of loudest spice mining
+        //move forward
+        //eat any tank/spice on collision
+    }
+
 }
